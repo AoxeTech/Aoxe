d1ab7bed31c6b2a8463b4ad04152f5fde8ac8865
Mutuduxf <aeondxf@live.com>
2018-04-20 15:08:55 +0800

    Recode demo

---
+++ Demo/ShippingHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShippingHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
