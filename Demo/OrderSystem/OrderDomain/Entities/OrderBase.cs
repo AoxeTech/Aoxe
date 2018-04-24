499848ff5eaa1e2be61d9352e7a5d63e70b49ff6
Mutuduxf <aeondxf@live.com>
2018-04-24 11:24:35 +0800

    Modify demos' folders

---
+++ Demo/OrderSystem/OrderDomain/Entities/OrderBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrderDomain.Entities
+{
+    public class OrderBase
+    {
+        public string Id { get; protected set; }
+        public DateTimeOffset CreateTimeOffset { get; protected set; }
+        public int UserId { get; protected set; }
+        public int Lenght { get; protected set; }
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+        public int Weight { get; protected set; }
+    }
+}
