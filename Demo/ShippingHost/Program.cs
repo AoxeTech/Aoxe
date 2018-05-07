81193e1ada8fe51123d362743e0d60c955e8536e
Mutuduxf <aeondxf@live.com>
2018-05-07 10:53:50 +0800

    Complete the Mongo/Redis/RabbitMq setting.

--- Demo/ShippingHost/Program.cs
+++ Demo/ShippingHost/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Zaabee.Mongo;
+using Zaabee.Mongo.Common;
+using Zaabee.Mongo.Core;
 using Zaaby;
+using Zaaby.Cache.RedisProvider;
+using Zaaby.MessageBus.Core;
+using Zaaby.MessageBus.RabbitMqProvider;
+using Zaaby.MessageBus.RabbitMqProvider.Json;
 
 namespace ShippingHost
 {
@@ -9,12 +16,28 @@ namespace ShippingHost
     {
         static void Main(string[] args)
         {
-            var configBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
-            var appServiceConfig = configBuilder.AddJsonFile("ApplicationService.json", true, true).Build();
-            
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("ApplicationService.json", true, true)
+                .AddJsonFile("Mongo.json", true, true)
+                .AddJsonFile("RabbitMQ.json", true, true)
+                .AddJsonFile("Redis.json", true, true);
+            var config = configBuilder.Build();
+
+            var appServiceConfig = config.GetSection("ZaabyApplication").Get<Dictionary<string, List<string>>>();
+            var mongoConfig = config.GetSection("ZaabeMongo").Get<MongoDbConfiger>();
+            var rabbitmqConfig = config.GetSection("ZaabyRabbitMQ").Get<MqConfig>();
+            var redisConfig = config.GetSection("ZaabyRedis").Get<RedisConfig>();
+
             ZaabyServer.GetInstance()
                 .UseZaabyRepository()
-                .UseZaabyApplicationService(appServiceConfig.Get<Dictionary<string,List<string>>>())
+                .UseZaabyDomainService()
+                .UseZaabyApplicationService(appServiceConfig)
+                .AddSingleton<IMongoDbRepository>(p => new MongoDbRepository(mongoConfig))
+                .AddSingleton<IHandler, ZabbyRabbitMqClient>(p =>
+                    new ZabbyRabbitMqClient(rabbitmqConfig, new Serializer()))
+                .AddSingleton<Zaaby.Cache.Core.IHandler, ZaabyRedisClient>(p =>
+                    new ZaabyRedisClient(redisConfig, new Zaaby.Cache.RedisProvider.Protobuf.Serializer()))
                 .UseUrls("http://*:5002")
                 .Run();
         }
