863607d2726bb35d5a3bffaa4877a7da1bec7b36
Mutuduxf <aeondxf@live.com>
2018-05-10 15:39:37 +0800

    develop

---
+++ Zaaby.Core/Application/IMessageHandler.cs
@@ -0,0 +1,9 @@
+﻿using Zaaby.Core.Infrastructure.EventBus;
+
+namespace Zaaby.Core.Application
+{
+    public interface IMessageHandler<in TMessage> where TMessage : IMessage
+    {
+        void Handle(TMessage message);
+    }
+}
