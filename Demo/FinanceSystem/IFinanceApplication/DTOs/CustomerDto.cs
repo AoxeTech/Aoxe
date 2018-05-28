589d424951af6eb76563d3319295d4c17a24149c
Mutuduxf <aeondxf@live.com>
2018-05-28 09:08:24 +0800

    Add server and client project

---
+++ Demo/FinanceSystem/IFinanceApplication/DTOs/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IFinanceApplication.DTOs
+{
+    public class CustomerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset CreateTime { get; set; }
+    }
+}
---
+++ Zaaby/WebApiResultFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Zaaby.Core;
+
+namespace Zaaby
+{
+    internal class WebApiResultFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception != null) return;
+            if (context.Result is ObjectResult objectResult)
+                context.Result = new JsonResult(new ZaabyDtoBase {Data = objectResult.Value, Status = Status.Success});
+        }
+    }
+}
