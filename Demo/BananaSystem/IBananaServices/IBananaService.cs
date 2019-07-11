449a3068930163792e781dc8ffd2140a2509a7d6
Mutuduxf <aeondxf@live.com>
2019-07-11 17:16:13 +0800

    Recode demo

---
+++ Demo/BananaSystem/IBananaServices/IBananaService.cs
@@ -0,0 +1,14 @@
+﻿using Interfaces;
+using System;
+
+namespace IBananaServices
+{
+    public interface IBananaService : ITest
+    {
+        Guid GetGuid();
+        string GetBananaMsg();
+        BananaDto ThrowBack(BananaDto dto);
+        void ThrowEx();
+        void TestAppleEx();
+    }
+}
