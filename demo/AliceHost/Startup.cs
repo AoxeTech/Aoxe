a612332f9d46a997d74bc0635242845a79869cfe
Mutuduxf <aeondxf@live.com>
2020-03-26 16:12:57 +0800

    Recode

--- demo/AliceHost/Startup.cs
+++ demo/AliceHost/Startup.cs
@@ -14,12 +14,12 @@ namespace AliceHost
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddZaaby<ITest>()
-                .UseZaabyClient(new Dictionary<string, List<string>>
-                {
-                    {"IBobServices", new List<string> {"http://localhost:5002"}},
-                    {"ICarolServices", new List<string> {"http://localhost:5003"}}
-                });
+            services.AddZaaby<ITest>();
+            services.UseZaabyClient(new Dictionary<string, List<string>>
+            {
+                {"IBobServices", new List<string> {"http://localhost:5002"}},
+                {"ICarolServices", new List<string> {"http://localhost:5003"}}
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- demo/CarolHost/Startup.cs
+++ demo/CarolHost/Startup.cs
@@ -15,12 +15,12 @@ namespace CarolHost
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddZaaby<ITest>()
-                .UseZaabyClient(new Dictionary<string, List<string>>
-                {
-                    {"IAliceServices", new List<string> {"http://localhost:5001"}},
-                    {"IBobServices", new List<string> {"http://localhost:5002"}}
-                });
+            services.AddZaaby<ITest>();
+            services.UseZaabyClient(new Dictionary<string, List<string>>
+            {
+                {"IAliceServices", new List<string> {"http://localhost:5001"}},
+                {"IBobServices", new List<string> {"http://localhost:5002"}}
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
