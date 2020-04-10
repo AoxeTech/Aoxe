c07b164b83f632f3f2577911aeea33b2eed79597
Mutuduxf <aeondxf@live.com>
2020-04-10 21:01:57 +0800

    Recode

--- demo/AliceHost/Startup.cs
+++ demo/AliceHost/Startup.cs
@@ -3,6 +3,7 @@ using Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Zaaby.Client;
 using Zaaby.Core;
 
@@ -20,12 +21,32 @@ namespace AliceHost
                 {"IBobServices", new List<string> {"http://localhost:5002"}},
                 {"ICarolServices", new List<string> {"http://localhost:5003"}}
             });
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("Alice", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Alice API",
+                    Description = "API for AliceHost",
+                    Contact = new OpenApiContact() {Name = "DuXiaoFei", Email = "aeondxf@live.com"}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseZaaby();
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            //Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint("/swagger/Alice/swagger.json", "Alice Docs");
+
+                option.RoutePrefix = string.Empty;
+                option.DocumentTitle = "Alice API";
+            });
         }
     }
 }
