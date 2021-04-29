77f8ce32c56f4601323f6a56db2156e966766703
XiaoFeiDu <romanchaos@163.com>
2021-04-29 14:44:56 +0800

    Debug

--- src/Zaaby.Service/ZaabyActionModelConvention.cs
+++ src/Zaaby.Service/ZaabyActionModelConvention.cs
@@ -22,7 +22,7 @@ namespace Zaaby.Service
             if (!_serviceType.IsAssignableFrom(action.Controller.ControllerType)) return;
 
             action.Selectors.Clear();
-            var route = _serviceType.GetCustomAttribute(typeof(Attribute), false) as RouteAttribute;
+            var route = _serviceType.GetCustomAttribute(typeof(RouteAttribute), false) as RouteAttribute;
             var template = $"{_serviceType.FullName?.Replace('.', '/')}/[action]";
             action.Selectors.Add(CreateSelector(route ?? new RouteAttribute(template) {Name = template}));
 
