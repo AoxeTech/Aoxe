5667b9e511db6d9c7805bc4db6a30be76125b371
XiaoFeiDu <romanchaos@163.com>
2021-08-10 15:27:56 +0800

    Fix reference

--- src/Zaaby/ZaabyHost.Extensions.Client.cs
+++ src/Zaaby/ZaabyHost.Extensions.Client.cs
@@ -7,7 +7,7 @@ namespace Zaaby
     public static partial class ZaabyHostExtensions
     {
         public static ZaabyHost UseZaabyClient(this ZaabyHost zaabyHost, Type serviceDefineType,
-            Dictionary<string, string> baseUrls) =>
-            zaabyHost.ConfigureServices(services => services.AddZaabyClient(serviceDefineType, baseUrls));
+            Dictionary<string, string> configUrls) =>
+            zaabyHost.ConfigureServices(services => services.AddZaabyClient(serviceDefineType, configUrls));
     }
 }
