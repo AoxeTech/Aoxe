f930d3a08f37085641285b67ab5329f2a223f668
XiaoFeiDu <romanchaos@163.com>
2022-02-21 17:19:23 +0800

    Add ZaabyHttpClientFormatterFactory.cs

---
+++ src/Client/Http/Formatter/Zaaby.Client.Http.Formatter/ZaabyHttpClientFormatterFactory.cs
@@ -0,0 +1,13 @@
+namespace Zaaby.Client.Http.Formatter;
+
+public class ZaabyHttpClientFormatterFactory
+{
+    public IZaabyHttpClientFormatter Create(ZaabyClientFormatterOptions options) =>
+        options.Serializer switch
+        {
+            ITextSerializer => new ZaabyHttpClientTextFormatter(options),
+            not null => new ZaabyHttpClientStreamFormatter(options),
+            _ => throw new ArgumentOutOfRangeException(nameof(options.Serializer),
+                $"options.Serializer must be {nameof(ITextSerializer)} or {nameof(IStreamSerializer)}.")
+        };
+}
