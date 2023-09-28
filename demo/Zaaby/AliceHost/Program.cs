195a71d118846cc793d084b1ab0059dfb667be91
Mutudu <aeondxf@live.com>
2023-09-28 14:16:43 +0800

    Upgrade nuget packages

--- demo/Zaaby/AliceHost/Program.cs
+++ demo/Zaaby/AliceHost/Program.cs
@@ -1,17 +1,26 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.Hosting;
 
 namespace AliceHost;
 
 public class Program
 {
-    public static void Main(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateDefaultBuilder(args);
+        builder.UseOrleans(silo =>
+        {
+            silo.UseLocalhostClustering()
+                .ConfigureLogging(logging => logging.AddConsole());
+        });
+        builder.ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
                 webBuilder.UseUrls("http://localhost:5001");
             })
             .Build()
             .Run();
+    }
 }
