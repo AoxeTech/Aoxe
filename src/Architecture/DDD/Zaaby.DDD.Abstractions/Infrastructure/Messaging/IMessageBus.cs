d686d40cc8083db34e8fee8cf4c99d1178a391eb
Mutudu <aeondxf@live.com>
2023-10-20 11:25:42 +0800

    Upgrade nuget packages

--- src/Architecture/DDD/Zaaby.DDD.Abstractions/Infrastructure/Messaging/IMessageBus.cs
+++ src/Architecture/DDD/Zaaby.DDD.Abstractions/Infrastructure/Messaging/IMessageBus.cs
@@ -3,5 +3,5 @@ namespace Zaaby.DDD.Abstractions.Infrastructure.Messaging;
 public interface IMessageBus
 {
     void Publish<T>(string topic, T message);
-    Task PublishAsync<T>(string topic, T message);
+    ValueTask PublishAsync<T>(string topic, T message);
 }
--- src/Architecture/DDD/Zaaby.DDD.MessageBus.ZeroMQ/MessageBus.cs
+++ src/Architecture/DDD/Zaaby.DDD.MessageBus.ZeroMQ/MessageBus.cs
@@ -15,7 +15,7 @@ public class MessageBus : IMessageBus, IDisposable
     public void Publish<T>(string topic, T message) =>
         _messageBus.Publish(topic, message);
 
-    public async Task PublishAsync<T>(string topic, T message) =>
+    public async ValueTask PublishAsync<T>(string topic, T message) =>
         await _messageBus.PublishAsync(topic, message);
 
     public void Dispose() =>
