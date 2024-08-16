9605ed415f76c246730ba43713c02449c745fcc8
Mutudu <aeondxf@live.com>
2024-08-16 21:48:46 +0800

    Remove unused using.

--- src/Server/CentralizedConfiguration/Consul/Aoxe.Extensions.Configuration.Consul/GlobalUsings.cs
+++ src/Server/CentralizedConfiguration/Consul/Aoxe.Extensions.Configuration.Consul/GlobalUsings.cs
@@ -1,7 +1,6 @@
 global using System;
 global using System.Collections.Generic;
 global using System.IO;
-global using System.Net.Http;
 global using System.Text;
 global using Aoxe.Extensions.Configuration.Flattener.Abstractions;
 global using Consul;
